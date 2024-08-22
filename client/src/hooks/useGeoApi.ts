ba1af4f9de766841578f6a17c29ceebb3f4a2b25
Maksym Ovsiannikov <c_movsiannikov@groupon.com>
2024-08-22 14:02:08 +0200

    adding logic of re-fetching the location with interval

--- client/src/hooks/useGeoApi.ts
+++ client/src/hooks/useGeoApi.ts
@@ -1,14 +1,34 @@
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {getApiResponses} from "../types/iss-api-responses";
 
 export default function useGeoApi(iteration = 10000) {
-    const [position, setPosition] = useState<getApiResponses>();
-
-    useEffect(() => {
-        fetch('http://localhost:3001/geo')
+    const [position, setPosition] = useState<getApiResponses | null>(null);
+    let intervalId: number | undefined;
+    const runFetch = useCallback(() => {
+        return fetch('http://localhost:3001/geo')
             .then(reps => reps.json())
             .then(data => setPosition(data))
-            .catch((err) => console.log(err));
+            .catch((err) => {
+                setPosition(null)
+                throw err;
+            });
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                await runFetch();
+                intervalId = window.setInterval(() => {
+                    runFetch();
+                }, iteration);
+            } catch {
+                return;
+            }
+        })()
+
+        return () => {
+            intervalId && clearInterval(intervalId);
+        }
     }, []);
 
     return position;
